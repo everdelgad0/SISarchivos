48dd49a70448f8648abf4cd8692371df60556c1b
Daniel Delgado <leinadreve001@gmail.com>
2025-12-16 10:42:31 -0400

    volver

--- SISarchivos/settings.py
+++ SISarchivos/settings.py
@@ -27,17 +27,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
 # Orígenes de confianza para la protección CSRF
 # Esto es necesario cuando se ejecuta detrás de un proxy o en Docker.
-CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
-    "http://0.0.0.0:8000",  # útil si el contenedor expone 0.0.0.0
-    "http://172.17.0.1:8000",  # IP típica del host visto desde Docker
-]
-
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://127.0.0.1:8000,http://localhost:8000', cast=Csv())
 
 
 # Application definition
